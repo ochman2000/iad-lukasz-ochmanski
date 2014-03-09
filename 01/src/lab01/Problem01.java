e90f5006b5bfb8551401f649368b0681cbd847f0
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-03-09 14:30:23 +0000

    Usunięte komentarze i warningi.

--- 01/src/lab01/Problem01.java
+++ 01/src/lab01/Problem01.java
@@ -1,9 +1,7 @@
 package lab01;
-import java.io.BufferedReader;
-import java.io.FileReader;
+
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Problem01 {
@@ -23,7 +21,6 @@ public class Problem01 {
 			outputStream = new FileWriter(filename);
 			for (int i = 0; i < amount; i++) {
 				double liczba = rn.nextDouble();
-//				System.out.println(liczba);
 				outputStream.write(liczba + "\r\n");
 			}
 			outputStream.close();
@@ -34,7 +31,6 @@ public class Problem01 {
 
 	public static void main(String[] args) {
 		Problem01 lab = new Problem01();
-//		lab.generateNumbers(20, "liczby.txt");
 		lab.generateNumbers(Integer.parseInt(args[0]), args[1]);
 	}
 }
--- 01/src/lab01/Problem02.java
+++ 01/src/lab01/Problem02.java
@@ -1,24 +1,24 @@
 package lab01;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Problem02 {
 
-	public Problem02() {	}
+	public Problem02() {
+	}
 
 	/*
 	 * Zadanie 2 Wczytaæ z pliku zbiór liczb i wczytaæ œredni¹ arytmetyczn¹.
 	 */
 	public Double getAvg(String filename) {
 		ArrayList<Double> list = new ArrayList<Double>();
-        BufferedReader inputStream = null;
-        double suma=0.0;
-        try {
-            inputStream = new BufferedReader(new FileReader(filename));
+		BufferedReader inputStream = null;
+		double suma = 0.0;
+		try {
+			inputStream = new BufferedReader(new FileReader(filename));
 			String l;
 			while ((l = inputStream.readLine()) != null) {
 				list.add(Double.parseDouble(l));
@@ -27,17 +27,15 @@ public class Problem02 {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-        for (Double x : list) {
-			suma=+x;
+		for (Double x : list) {
+			suma = +x;
 		}
-		return suma/(list.size());
+		return suma / (list.size());
 	}
 
 	public static void main(String[] args) {
 		Problem02 lab = new Problem02();
-//		Double srednia = lab.getAvg("liczby.txt");
-//		System.out.println(srednia);
 		Double srednia = lab.getAvg(args[0]);
-		System.out.println("\r\nSrednia arytmetyczna: "+srednia);
+		System.out.println("\r\nSrednia arytmetyczna: " + srednia);
 	}
 }
