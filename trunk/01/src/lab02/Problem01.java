d47bcf669f01e8707d3ec89a3c21ec0751c10c4a
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-03-15 11:50:20 +0000

    Mikrozadanie 1 działa

---
+++ trunk/01/src/lab02/Problem01.java
@@ -0,0 +1,40 @@
+package lab02;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Problem01 {
+	
+	private static double w1 = -1.0;
+	private static double w2 = -1.0;
+
+	public static void main(String[] args) {
+		
+		File file = new File("dane0/a");
+		int minusowe=0;
+		int plusowe=0;
+		
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while (sc.hasNext()) {
+			double a = sc.nextDouble();
+			double b = sc.nextDouble();
+			double x = Math.signum(a*(w1)+b*(w2));
+			if (x<0.0)
+				minusowe++;
+			if (x>0.0) {
+				plusowe++;
+			} else {
+//				System.err.println("Coś nie śmiga...");
+			}
+				
+		}
+		System.out.println("Liczba -1: "+minusowe);
+		System.out.println("Liczba  1: "+plusowe);
+	}
+}
