dfa5b551ead00e63dd06eda926cda124e791cf86
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-04-06 06:55:17 +0000

    zadanie od kumpla

--- trunk/01/src/lab02/Problem01.java
+++ trunk/01/src/lab02/Problem01.java
@@ -6,9 +6,15 @@ import java.util.Scanner;
 
 public class Problem01 {
 	
-	private static double w1 = -1.0;
-	private static double w2 = -1.0;
+	private static double w1 = -1.0; //to będzie się zmieniało
+	private static double w2 = -1.0; //
+	//jak przestanie się zmieniać
 
+	//wartośc oczekiwana dla pliku a wynosi 1
+	
+	//wartość oczekiwana dla pliku b wynosi -1
+	
+	
 	public static void main(String[] args) {
 		
 		File file = new File("dane0/a");
@@ -32,7 +38,6 @@ public class Problem01 {
 			} else {
 //				System.err.println("Coś nie śmiga...");
 			}
-				
 		}
 		System.out.println("Liczba -1: "+minusowe);
 		System.out.println("Liczba  1: "+plusowe);
