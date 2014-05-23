158ec85d603f74e29894ab8ad94710619c76c88c
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-05-23 23:28:29 +0000

    Poprawne ustawianie biasu.

--- 02/src/pl/lodz/p/iad/Sample.java
+++ 02/src/pl/lodz/p/iad/Sample.java
@@ -86,10 +86,11 @@ public class Sample {
 		}
 		
 		network.addLayer(layer0);
+		layer1.setBias(-7.0);
 		network.addLayer(layer1);
+		layer2.setBias(-7.0);
 		network.addLayer(layer2);
 		
-		network.setBias(-7.0);
 		network.setMomentum(1.0);
 		
 		return network;
