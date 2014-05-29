6f197344ede84f3ff2c07a3c4905855b210c9892
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-05-29 23:31:35 +0000

    Dodany Mean Sqaure Error

--- trunk/02/src/pl/lodz/p/iad/XOR.java
+++ trunk/02/src/pl/lodz/p/iad/XOR.java
@@ -6,11 +6,6 @@ import pl.lodz.p.iad.structure.Neuron;
 
 public class XOR {
 	
-	public static int NUMBER_OF_CLASSES = 2;
-	public static int NUMBER_OF_LAYERS = 3;
-	public static double LEARN_RATE = 0.7;
-	public static double MOMENTUM = 0.9;
-	
 	public static double XOR_INPUT[][] = {
 		{ 0.0, 0.0 }, 
 		{ 1.0, 0.0 },
@@ -25,14 +20,14 @@ public class XOR {
 	
 	private void initializeStructure() {
 		//INPUT LAYER
-		Neuron neuron_0_0 = new InputLayerNeuron(1,3);
-		Neuron neuron_0_1 = new InputLayerNeuron(1,3);
+		Neuron neuron_0_0 = new InputLayerNeuron();
+		Neuron neuron_0_1 = new InputLayerNeuron();
 		//HIDDEN LAYER
-		Neuron neuron_1_0 = new HiddenLayerNeuron(2,1);
-		Neuron neuron_1_1 = new HiddenLayerNeuron(2,1);
-		Neuron neuron_1_2 = new HiddenLayerNeuron(2,1);
+		Neuron neuron_1_0 = new HiddenLayerNeuron();
+		Neuron neuron_1_1 = new HiddenLayerNeuron();
+		Neuron neuron_1_2 = new HiddenLayerNeuron();
 		//OUTPUT LAYER
-		Neuron neuron_2_0 = new HiddenLayerNeuron(3,1);
+		Neuron neuron_2_0 = new HiddenLayerNeuron();
 		
 		//CONNECTIONS
 		neuron_0_0.addNeuronOut(neuron_1_0);
