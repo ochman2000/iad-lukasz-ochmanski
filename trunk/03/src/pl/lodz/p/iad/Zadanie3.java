77cfddb5891c0e23128875a3c93105d3f7096f67
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-07-01 22:45:00 +0000

    Initial commit.

---
+++ trunk/03/src/pl/lodz/p/iad/Zadanie3.java
@@ -0,0 +1,10 @@
+package pl.lodz.p.iad;
+
+public class Zadanie3 {
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ trunk/03/src/pl/lodz/p/iad/structure/Mapa.java
@@ -0,0 +1,5 @@
+package pl.lodz.p.iad.structure;
+
+public class Mapa {
+
+}
