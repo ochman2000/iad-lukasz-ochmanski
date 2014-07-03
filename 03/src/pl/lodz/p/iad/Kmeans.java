501cc335262bf624b13e63119dbd873827066df1
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-07-03 22:52:58 +0000

    Grupowanie zakończone powodzeniem.

--- 03/src/pl/lodz/p/iad/Kmeans.java
+++ 03/src/pl/lodz/p/iad/Kmeans.java
@@ -10,11 +10,13 @@ import pl.lodz.p.iad.structure.Point;
 public class Kmeans {
 	
 	private static final int PODZBIORY = 3;
+	private Mapa mapa;
+	private Set<Point> centroidy;
 
 	public Kmeans() {
-		Mapa mapa = new Mapa();
+		mapa = new Mapa();
 		Random rnd = new Random();
-		Set<Point> centroidy = new HashSet<Point>(PODZBIORY);
+		centroidy = new HashSet<Point>(PODZBIORY);
 		
 		//LOSUJ K CENTROIDÓW
 		while (centroidy.size()<PODZBIORY) {
@@ -25,12 +27,38 @@ public class Kmeans {
 				centroidy.add(centroid);
 			}
 		}
-		System.out.println("Udało się.");
+		System.out.println("Losowanie zakończone powodzeniem.");
 		for (Point point : mapa) {
 			if (point.isCentroid())
 			System.out.println(point);
 		}
+		
+		grupujPunkty();
+		System.out.println("Grupowanie centroidów zakończone powodzeniem.");
+		
 	}
+	
+	/**
+	 * Dla wszystkich punktów, które nie są centroidami, sprawdź odległości tych punktów
+	 * od każdego centroida i wybierz ten centroid, którego odległość od tego punktu jest
+	 * najmniejsza. Następnie przypisz ten punkt do grupy tego centroida.
+	 * Złożoność obliczeniowa: O(p*c), gdzie p to liczba punktów (10000) a c to liczba
+	 * centroidów (3).
+	 */
+	private void grupujPunkty() {
+		for (Point punkt : mapa) {
+			if (punkt.isCentroid()==false) {
+				double min = Double.MAX_VALUE;
+				for (Point centroid : centroidy) {
+					double dist = punkt.getDistanceFrom(centroid);
+					if (dist<min) {
+						punkt.setGroup(centroid);
+					}
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		new Kmeans();
 	}
