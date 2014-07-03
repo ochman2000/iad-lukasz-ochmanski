a559d9e1d69f633c2bc2225f868ae2c00c5d3ea7
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-07-03 23:12:28 +0000

    Dowolna liczba wymiarów.

--- 03/test/pl/lodz/p/iad/structure/PointTest.java
+++ 03/test/pl/lodz/p/iad/structure/PointTest.java
@@ -33,4 +33,20 @@ public class PointTest {
 		
 		assertTrue(p2.isCentroid());
 	}
+	
+	@Test
+	public void test03() {
+		Point p1 = new Point(2);
+		p1.addCoordinate(0, 3.0);
+		p1.addCoordinate(1, 0.0);
+		p1.addCoordinate(2, 0.0);
+		
+		Point p2 = new Point(2);
+		p2.addCoordinate(0, 0);
+		p2.addCoordinate(1, -4.0);
+		p2.addCoordinate(2, 0.0);
+		p2.setGroup(p2);
+		
+		assertEquals(5.0, p2.getDistanceFrom(p1), 0.00001);
+	}
 }
