3137314e5b0f6a261f046fa1b3ad83cd5b41913d
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-07-22 21:13:11 +0000

    Nowy konstruktor Kmeans()

--- 03/src/pl/lodz/p/iad/Zadanie3.java
+++ 03/src/pl/lodz/p/iad/Zadanie3.java
@@ -1,30 +1,15 @@
 package pl.lodz.p.iad;
 
 import java.util.Arrays;
-import java.util.List;
-
-import pl.lodz.p.iad.structure.Zbior;
 
 public class Zadanie3 {
 
 	public static void main(String[] args) {
 
-		new Kmeans(new Zbior() {
-			public List<Integer> getColumnIndices() {
-				return Arrays.asList(0, 1);
-			}
-		});
+		new Kmeans(Arrays.asList(0, 1));
 		
-//		new Kmeans(new Zbior() {
-//			public List<Integer> getColumnIndices() {
-//				return Arrays.asList(2, 3, 4);
-//			}
-//		});
-//		
-//		new Kmeans(new Zbior() {
-//			public List<Integer> getColumnIndices() {
-//				return Arrays.asList(5, 6, 7);
-//			}
-//		});
+		new Kmeans(Arrays.asList(2, 3, 4));
+		
+		new Kmeans(Arrays.asList(5, 6, 7));
 	}
 }
