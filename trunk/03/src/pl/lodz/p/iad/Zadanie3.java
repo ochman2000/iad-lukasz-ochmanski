99968937f560014daa16116f16a911f919889a32
ochman2000@gmail.com <ochman2000@gmail.com@7972061b-1985-0ceb-1033-4d2f64d1dae8>
2014-07-22 20:39:06 +0000

    Skrócenie zapisu.

--- trunk/03/src/pl/lodz/p/iad/Zadanie3.java
+++ trunk/03/src/pl/lodz/p/iad/Zadanie3.java
@@ -1,23 +1,30 @@
 package pl.lodz.p.iad;
 
-
 import java.util.Arrays;
 import java.util.List;
 
-import pl.lodz.p.iad.structure.Mapa;
 import pl.lodz.p.iad.structure.Zbior;
 
 public class Zadanie3 {
-	
-	
+
 	public static void main(String[] args) {
-	
-		Mapa mapa1 = new Mapa(new Zbior() {
-			@Override
+
+		new Kmeans(new Zbior() {
 			public List<Integer> getColumnIndices() {
-				return Arrays.asList(0,1);
+				return Arrays.asList(0, 1);
 			}
 		});
-		new Kmeans(mapa1);
+		
+//		new Kmeans(new Zbior() {
+//			public List<Integer> getColumnIndices() {
+//				return Arrays.asList(2, 3, 4);
+//			}
+//		});
+//		
+//		new Kmeans(new Zbior() {
+//			public List<Integer> getColumnIndices() {
+//				return Arrays.asList(5, 6, 7);
+//			}
+//		});
 	}
 }
